3b1d23681b71984676cd2c6e4bc611d39b66cb0b
lengshengren <lengshengren@lengshengrentekiiMac.local>
2013-10-17 15:14:33 +0800

    Initial Commit

---
+++ CoreDataDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CoreDataDemo
+//
+//  Created by lengshengren on 13-10-17.
+//  Copyright (c) 2013年 lengshengren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
